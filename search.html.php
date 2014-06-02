cc6eb43374a0ce8bae48467f50910557ca877357
Randy <randya.cramer@gmail.com>
2014-06-02 18:28:03 -0400

    Update search.html.php

--- search.html.php
+++ search.html.php
@@ -1,22 +1,13 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <form action="index.php" method="post">
-            <input type="text" placeholder="Search Stuff" />
+        <form action="" method="post">
+            <input type="text" name="search" placeholder="Search Stuff" />
             <input type="submit" value="Search" /><br />
-            <?php
-            echo $gotstuff . '<br />';
-            echo $listings;
-            ?>
         </form>
     </body>
 </html>
