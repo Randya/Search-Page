537576809b36b119a6ef29fa5656f9ec98c97da0
Randy <randya.cramer@gmail.com>
2014-06-02 23:02:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,20 +8,24 @@ $pdo->exec('SET NAMES "utf8"');
     include 'error.html.php';
     exit();
 }
+$message = 'Enter your search terms above.';
 if (isset($_POST['search'])){
+    
     $searchterm = $_POST['search'];
     $sql = 'SELECT searchtext FROM list
         WHERE searchtext LIKE "%' . $searchterm . '%"';
     $result = $pdo->query($sql);
-
 foreach($result as $results){
     $listings[] = $results['searchtext'];
 }
-
+if(!empty($listings)){
 include 'results.html.php';
 exit();
 }
-
+else{
+    $message = 'No search terms found.';
+}
+}
 include 'search.html.php';
 
 
